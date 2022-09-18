cccda829a3e8e332a6bc5ac531642772bba24bc6
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-09-18 23:47:49 +0200

    Add files via upload

---
+++ inicijalnaProvjera_1.dio-7.zadatak/inicijalnaProvjera_1.dio-7.zadatak/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalnaProvjera_1.dio_7.zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prim=false;
+            int primBr = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                
+                    for (int j = 2; j <= 100; j++)
+                    {
+                        if (i != j && i % j == 0)
+                        {
+                            prim = false;
+                            break;
+                        }
+                    }
+                    if (prim)
+                    {
+                        primBr++;
+                    }
+                    prim = true;
+                
+            }
+
+            Console.WriteLine(primBr);
+            Console.ReadKey();
+        }
+    }
+}
